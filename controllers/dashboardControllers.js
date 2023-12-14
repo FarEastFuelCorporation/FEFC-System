ea5728a4ef7a83d4a36dc08b51ddf9606f21af5d
jimfalar@gmail.com <jimfalar@gmail.com>
2023-12-14 09:38:58 +0800

    Update Files

--- controllers/dashboardControllers.js
+++ controllers/dashboardControllers.js
@@ -28,27 +28,43 @@ async function bookedTransactionsController(req, res) {
     }
 }
 
-// Clients controller
+// clientsController function
 async function clientsController(req, res) {
     try {
         // Fetch all clients from the database
         const clients = await Client.findAll();
 
-        // Get the page number from the query parameter (default to 1 if not provided)
+        // Get the page number, entries per page, and search query from the query parameters
         const currentPage = parseInt(req.query.page) || 1;
+        const entriesPerPage = parseInt(req.query.entriesPerPage) || 10;
+        const searchQuery = req.query.search || ''; // Default to an empty string if no search query
 
-        // Fetch the total number of clients (for pagination)
-        const totalClientsCount = await Client.count();
+        // Additional logic to filter clients based on the search query
+        const filteredClients = clients.filter(client => {
+            // Customize this logic based on how you want to perform the search
+            return (
+                client.clientName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+                client.address.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+        });
 
-        // Calculate total pages based on the total number of clients and entries per page
-        const totalEntriesPerPage = 10; // You can adjust this based on your preference
-        const totalPages = Math.ceil(totalClientsCount / totalEntriesPerPage);
+        // Calculate total pages based on the total number of filtered clients and entries per page
+        const totalFilteredClients = filteredClients.length;
+        const totalPages = Math.ceil(totalFilteredClients / entriesPerPage);
+
+        // Implement pagination and send the filtered clients to the view
+        const paginatedClients = filteredClients.slice(
+            (currentPage - 1) * entriesPerPage,
+            currentPage * entriesPerPage
+        );
 
         // Render the 'marketing/clients' view and pass the necessary data
         res.render('marketing/clients', {
-            clients,
+            clients: paginatedClients,
             currentPage,
             totalPages,
+            entriesPerPage,
+            searchQuery, // Pass the search query to update the search input on the client side
             // ... other data
         });
     } catch (error) {
