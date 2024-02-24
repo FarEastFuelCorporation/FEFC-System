b19a347d0eb3ca9a0949ca570ad7397e7fc47f17
jimfalar@gmail.com <jimfalar@gmail.com>
2024-02-24 13:20:06 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,12 +3,17 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize({
+// const sequelize = new Sequelize({
+//     dialect: 'mysql',
+//     host: process.env.DB_HOST,
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DBNAME,
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
+    logging: console.log, // or false to disable logging
 });
 
 sequelize.options.logging = console.log;
