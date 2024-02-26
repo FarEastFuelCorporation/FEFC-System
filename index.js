119d0bb91f7f001ceb823cb4192e0a260c053a1a
jimfalar@gmail.com <jimfalar@gmail.com>
2024-02-26 10:27:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // index.js
 
-require('dotenv').config();
-require('events').EventEmitter.defaultMaxListeners = 25;
+require("dotenv").config();
+require("events").EventEmitter.defaultMaxListeners = 25;
 
-const express = require('express');
-const session = require('express-session');
-const multer = require('multer');
-const path = require('path');
-const bodyParser = require('body-parser');
-const sequelize = require('./config/config');
+const express = require("express");
+const session = require("express-session");
+const multer = require("multer");
+const path = require("path");
+const bodyParser = require("body-parser");
+const sequelize = require("./config/config");
 
 // Import utility functions and models
-require('./utils/associations');
+require("./utils/associations");
 
 // Set up storage for multer
 const storage = multer.memoryStorage();
@@ -21,76 +21,81 @@ const app = express();
 const port = process.env.PORT;
 
 // Set up EJS as the view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Serve Bootstrap files from the 'node_modules' folder
-app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use(
+  "/bootstrap",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist"))
+);
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
 // Set Cache-Control header to prevent caching
 app.use((req, res, next) => {
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    next();
+  res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+  next();
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Use express-session middleware
-app.use(session({
-    secret: 'your-secret-key',
+app.use(
+  session({
+    secret: "your-secret-key",
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: false, // Use only with HTTPS
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 10000, // Session expiration time
+      secure: false, // Use only with HTTPS
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 10000, // Session expiration time
     },
-}));
+  })
+);
 
 // Function to initialize the application
 async function initializeApp() {
-    try {
-        console.log('Syncing models to the database...');
-        // await sequelize.sync({  });
-        await sequelize.sync({ alter: false });
-        console.log('Models synced successfully.');
-    } catch (error) {
-        console.error('Error syncing models:', error);
-    }
+  try {
+    console.log("Syncing models to the database...");
+    // await sequelize.sync({  });
+    await sequelize.sync({ alter: false });
+    console.log("Models synced successfully.");
+  } catch (error) {
+    console.error("Error syncing models:", error);
+  }
 }
 
 // Call the function to initialize the application
 initializeApp();
 
 // Middleware to check authentication
-const { isAuthenticated } = require('./middlewares/auth');
-app.use('/requests', isAuthenticated);
-app.use('/marketing_dashboard', isAuthenticated);
-app.use('/dispatching_dashboard', isAuthenticated);
+const { isAuthenticated } = require("./middlewares/auth");
+app.use("/requests", isAuthenticated);
+app.use("/marketing_dashboard", isAuthenticated);
+app.use("/dispatching_dashboard", isAuthenticated);
 
 // Include your routes
-const authRoutes = require('./routes/auth');
-const othersRoutes = require('./routes/others');
-const requestsRoutes = require('./routes/requests');
-const marketingDashboardRoutes = require('./routes/marketing_dashboard');
-const dispatchingDashboardRoutes = require('./routes/dispatching_dashboard');
-const receivingDashboardRoutes = require('./routes/receiving_dashboard');
-const { error404Controller } = require('./controllers/othersController');
+const authRoutes = require("./routes/auth");
+const othersRoutes = require("./routes/others");
+const requestsRoutes = require("./routes/requests");
+const marketingDashboardRoutes = require("./routes/marketing_dashboard");
+const dispatchingDashboardRoutes = require("./routes/dispatching_dashboard");
+const receivingDashboardRoutes = require("./routes/receiving_dashboard");
+const { error404Controller } = require("./controllers/othersController");
 
 app.use(authRoutes);
 app.use(othersRoutes);
-app.use('/requests', requestsRoutes);
-app.use('/marketing_dashboard', marketingDashboardRoutes);
-app.use('/dispatching_dashboard', dispatchingDashboardRoutes);
-app.use('/receiving_dashboard', receivingDashboardRoutes);
+app.use("/requests", requestsRoutes);
+app.use("/marketing_dashboard", marketingDashboardRoutes);
+app.use("/dispatching_dashboard", dispatchingDashboardRoutes);
+app.use("/receiving_dashboard", receivingDashboardRoutes);
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(error404Controller);
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on  port ${port}`);
+  console.log(`Server is running on  port ${port}`);
 });
