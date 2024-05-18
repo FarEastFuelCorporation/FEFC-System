c9b209e7ff573efa0b39327668a71aaa94fe0b8c
jimfalar@gmail.com <jimfalar@gmail.com>
2024-05-18 14:37:59 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,6 +5,12 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL,{
     dialect: 'mysql',
+    pool: {
+        max: 10, // Maximum number of connections
+        min: 0,  // Minimum number of connections
+        acquire: 30000, // Maximum time in milliseconds to acquire a connection before throwing an error
+        idle: 10000 // Maximum time in milliseconds that a connection can be idle before being released
+    }
 });
 
 sequelize.options.logging = console.log;
